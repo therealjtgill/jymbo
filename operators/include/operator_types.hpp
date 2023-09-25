7887855f2ef2c4e468bf75ff610eab6efb910471
therealjtgill <therealjtgill@gmail.com>
2023-09-25 18:59:17 -0400

    Added enumerations for the preliminary set of supported functions.

---
+++ operators/include/operator_types.hpp
@@ -0,0 +1,30 @@
+#ifndef OPERATOR_TYPES_HEADER
+#define OPERATOR_TYPES_HEADER
+
+namespace jymbo
+{
+
+namespace types
+{
+
+   enum class enumOperatorType_t
+   {
+      kEqual = 0,
+      kAddition = 1,
+      kSubtraction = 2,
+      kMultiplication = 3,
+      kDivision = 4,
+      kNaturalLog = 5,
+      kNaturalExponent = 6,
+      kSine = 7,
+      kCosine = 8,
+      kTangent = 9,
+      kPower = 10,
+      kExponentiation = 11,
+   };
+
+}
+
+}
+
+#endif
