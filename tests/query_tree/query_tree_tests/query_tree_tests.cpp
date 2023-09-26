664c5a64c1480c7f591ebbf00bf4b778f124cc1f
therealjtgill <therealjtgill@gmail.com>
2023-09-26 09:10:28 -0400

    Made the query tree test instantiate a query tree.

--- tests/query_tree/query_tree_tests/query_tree_tests.cpp
+++ tests/query_tree/query_tree_tests/query_tree_tests.cpp
@@ -4,7 +4,11 @@
 
 #include "catch.hpp"
 
-TEST_CASE( "blank", "[symbols]" )
+TEST_CASE( "query_tree_instantiation", "[QueryTree]" )
 {
+   jymbo::types::queryNode_t q_node;
+   q_node.nodeType = jymbo::types::enumQueryNodeType_t::kOperator;
+   q_node.op = jymbo::types::enumOperatorType_t::kEqual;
+   jymbo::types::QueryTree q_tree(q_node);
    REQUIRE( true );
 }
