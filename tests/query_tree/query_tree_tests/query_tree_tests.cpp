6e14921a43cfff9a314aa03c4b279cdd1a7bdfa6
therealjtgill <therealjtgill@gmail.com>
2023-09-26 18:10:45 -0400

    The variable 'x' had a number attached to it, which didn't make any sense. Added a cubic polynomial 'test'.

--- tests/query_tree/query_tree_tests/query_tree_tests.cpp
+++ tests/query_tree/query_tree_tests/query_tree_tests.cpp
@@ -30,3 +30,17 @@ TEST_CASE( "quadratic tree instantiation", "[QueryTree]")
 
    REQUIRE( true );
 }
+
+TEST_CASE( "cubic tree instantiation", "[QueryTree]")
+{
+   jymbo::types::queryNode_t q_node;
+   q_node.nodeType = jymbo::types::enumQueryNodeType_t::kOperator;
+   q_node.op = jymbo::types::enumOperatorType_t::kEqual;
+   jymbo::types::QueryTree q_tree(q_node);
+
+   test_utils::polynomialQueryTree(3, q_tree);
+
+   query_tree::print(q_tree);
+
+   REQUIRE( true );
+}
--- tests/test_utils/polynomial_query_tree.cpp
+++ tests/test_utils/polynomial_query_tree.cpp
@@ -69,7 +69,7 @@ namespace test_utils
 
       jymbo::types::queryNode_t x_sym_node;
       x_sym_node.nodeType = jymbo::types::enumQueryNodeType_t::kSymbol;
-      std::snprintf(x_sym_node.symbol.name, 16, "x%i", n);
+      std::snprintf(x_sym_node.symbol.name, 16, "x");
       x_sym_node.symbol.uid = 1;
       x_sym_node.symbol.val = 0.f;
       x_sym_node.symbol.symbolType = jymbo::types::enumSymbolType_t::kIndependent;
