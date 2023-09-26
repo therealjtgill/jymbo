29f3e744499687670e6ea5abe75e5623e391c0ab
therealjtgill <therealjtgill@gmail.com>
2023-09-26 16:59:20 -0400

    Added the query tree subdirectory to the top-level Cmake folder.

--- tests/query_tree/query_tree_tests/query_tree_tests.cpp
+++ tests/query_tree/query_tree_tests/query_tree_tests.cpp
@@ -2,6 +2,8 @@
 
 #include "polynomial_query_tree.hpp"
 
+#include "query_tree.hpp"
+
 #define CATCH_CONFIG_MAIN
 
 #include "catch.hpp"
@@ -17,5 +19,14 @@ TEST_CASE( "query_tree_instantiation", "[QueryTree]" )
 
 TEST_CASE( "quadratic tree instantiation", "[QueryTree]")
 {
+   jymbo::types::queryNode_t q_node;
+   q_node.nodeType = jymbo::types::enumQueryNodeType_t::kOperator;
+   q_node.op = jymbo::types::enumOperatorType_t::kEqual;
+   jymbo::types::QueryTree q_tree(q_node);
 
+   test_utils::polynomialQueryTree(2, q_tree);
+
+   query_tree::print(q_tree);
+
+   REQUIRE( true );
 }
