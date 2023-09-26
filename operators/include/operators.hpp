1a20ce03ba7db07a0f5f924113dba6a1a07c3169
therealjtgill <therealjtgill@gmail.com>
2023-09-26 16:42:18 -0400

    Started adding a function that converts an operator enumeration to a string.

---
+++ operators/include/operators.hpp
@@ -0,0 +1,21 @@
+#ifndef OPERATORS_HEADER
+#define OPERATORS_HEADER
+
+#include "jymbo_types.hpp"
+
+#include <string>
+
+namespace jymbo
+{
+   std::string operatorToString(const types::enumOperatorType_t op)
+   {
+      switch(op)
+      {
+         case 
+      }
+
+      return std::string("no-operation-found");
+   }
+}
+
+#endif
