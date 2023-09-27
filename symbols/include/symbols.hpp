e49c329af41486f822815aa75a480192690de6e3
Jonathan Gill <jtgill@email.arizona.edu>
2023-09-26 21:16:15 -0400

    Added helper functions to initialize symbol types (because who needs constructors?).

--- symbols/include/symbols.hpp
+++ symbols/include/symbols.hpp
@@ -1,9 +1,24 @@
 #ifndef SYMBOLS_HEADER
 #define SYMBOLS_HEADER
 
+#include "jymbo_types.hpp"
+
 namespace jymbo
 {
-   
+   jymbo::types::symbol_t initializeSymbol(
+      const char * name,
+      const int uid,
+      const float val,
+      const jymbo::types::enumSymbolType_t symbol_type
+   );
+
+   jymbo::types::symbol_t initializeSymbol(
+      const char * name,
+      const int suffix,
+      const int uid,
+      const float val,
+      const jymbo::types::enumSymbolType_t symbol_type
+   );
 }
 
 #endif
