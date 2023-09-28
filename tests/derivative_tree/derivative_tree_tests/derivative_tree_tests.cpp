66b8f6e89eb207f9a9de060d54b7eb0b844b514e
therealjtgill <therealjtgill@gmail.com>
2023-09-28 11:50:01 -0400

    Added a test for the derivative of y = a * x.

--- tests/derivative_tree/derivative_tree_tests/derivative_tree_tests.cpp
+++ tests/derivative_tree/derivative_tree_tests/derivative_tree_tests.cpp
@@ -24,9 +24,9 @@ TEST_CASE( "print derivative tree", "[DerivativeTree]" )
    d_node.op = jymbo::types::enumOperatorType_t::kEqual;
    jymbo::types::DerivativeTree d_tree(d_node);
 
-   jymbo::types::derivativeNode_t q_y_node;
-   q_y_node.nodeType = jymbo::types::enumDerivativeNodeType_t::kSymbol;
-   q_y_node.symbol = jymbo::initializeSymbol(
+   jymbo::types::derivativeNode_t d_y_node;
+   d_y_node.nodeType = jymbo::types::enumDerivativeNodeType_t::kSymbol;
+   d_y_node.symbol = jymbo::initializeSymbol(
       "y", 0, 0.f, jymbo::types::enumSymbolType_t::kDependent
    );
 
@@ -42,12 +42,59 @@ TEST_CASE( "print derivative tree", "[DerivativeTree]" )
    right_query_ref.nodeType = jymbo::types::enumDerivativeNodeType_t::kReference;
    right_query_ref.qNodeId = 1;
 
-   d_tree.addChild(d_tree.getRootId(), q_y_node);
+   d_tree.addChild(d_tree.getRootId(), d_y_node);
    const int divide_op_id = d_tree.addChild(d_tree.getRootId(), divide_op);
    d_tree.addChild(divide_op_id, left_query_ref);
    d_tree.addChild(divide_op_id, right_query_ref);
 
    derivative_tree::print(d_tree);
+}
+
+TEST_CASE( "take derivative of multiplication", "[DerivativeTree]" )
+{
+   jymbo::types::queryNode_t q_node;
+   q_node.nodeType = jymbo::types::enumQueryNodeType_t::kOperator;
+   q_node.op = jymbo::types::enumOperatorType_t::kEqual;
+   jymbo::types::QueryTree q_tree(q_node);
+
+   jymbo::types::queryNode_t q_y_node;
+   q_y_node.nodeType = jymbo::types::enumQueryNodeType_t::kSymbol;
+   q_y_node.symbol = jymbo::initializeSymbol(
+      "y", 0, 0.f, jymbo::types::enumSymbolType_t::kDependent
+   );
+
+   jymbo::types::queryNode_t mult_op;
+   mult_op.nodeType = jymbo::types::enumQueryNodeType_t::kOperator;
+   mult_op.op = jymbo::types::enumOperatorType_t::kMultiplication;
+
+   jymbo::types::queryNode_t q_x_node;
+   q_x_node.nodeType = jymbo::types::enumQueryNodeType_t::kSymbol;
+   q_x_node.symbol = jymbo::initializeSymbol(
+      "x", 0, 0.f, jymbo::types::enumSymbolType_t::kIndependent
+   );
+
+   jymbo::types::queryNode_t q_a_node;
+   q_a_node.nodeType = jymbo::types::enumQueryNodeType_t::kSymbol;
+   q_a_node.symbol = jymbo::initializeSymbol(
+      "a", 0, 0.f, jymbo::types::enumSymbolType_t::kParameter
+   );
+
+   q_tree.addChild(q_tree.getRootId(), q_y_node);
+   const int mult_id = q_tree.addChild(q_tree.getRootId(), mult_op);
+   q_tree.addChild(mult_id, q_x_node);
+   q_tree.addChild(mult_id, q_a_node);
+
+   query_tree::print(q_tree);
+
+   jymbo::types::derivativeNode_t d_node;
+   d_node.nodeType = jymbo::types::enumDerivativeNodeType_t::kOperator;
+   d_node.op = jymbo::types::enumOperatorType_t::kEqual;
+   jymbo::types::DerivativeTree d_tree(d_node);
+
+   derivative_tree::derivatize(q_tree, d_tree);
+
+   derivative_tree::print(d_tree);
+
 }
 
 TEST_CASE( "take derivative of linear equation", "[DerivativeTree]" )
