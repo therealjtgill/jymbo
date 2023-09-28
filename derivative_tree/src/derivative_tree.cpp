bce384b9de01be09d408251d8384c4638b3c04e4
therealjtgill <therealjtgill@gmail.com>
2023-09-28 10:50:38 -0400

    Started adding the DFS function to take derivatives of a query tree.

--- derivative_tree/src/derivative_tree.cpp
+++ derivative_tree/src/derivative_tree.cpp
@@ -1,4 +1,6 @@
 #include "derivative_tree.hpp"
+#include "operators.hpp"
+#include "symbols.hpp"
 
 namespace derivative_tree
 {
@@ -74,5 +76,53 @@ namespace derivative_tree
       }
 
       std::cout << out_string << "\n";
+   }
+
+   void derivatize(
+      const jymbo::types::QueryTree & q_tree,
+      jymbo::types::DerivativeTree & d_tree
+   )
+   {
+      std::vector<int> frontier;
+
+      frontier.reserve(q_tree.size());
+
+      jymbo::types::derivativeNode_t d_root_node;
+      d_root_node.nodeType = jymbo::types::enumDerivativeNodeType_t::kOperator;
+      d_root_node.op = jymbo::types::enumOperatorType_t::kEqual;
+
+      jymbo::types::derivativeNode_t d_y_node;
+      d_y_node.nodeType = jymbo::types::enumDerivativeNodeType_t::kSymbol;
+      d_y_node.symbol = jymbo::initializeSymbol(
+         "y'", 0, 0.f, jymbo::types::enumSymbolType_t::kDependent
+      );
+
+      jymbo::types::derivativeNode_t rhs_q_ref_node;
+      rhs_q_ref_node.nodeType = jymbo::types::enumDerivativeNodeType_t::kReference;
+      rhs_q_ref_node.qNodeId = q_tree.getNode(q_tree.getRootId()).childNodeIds[1];
+
+      d_tree.setRoot(d_root_node);
+      const int d_root_id = d_tree.getRootId();
+
+      d_tree.addChild(d_root_id, d_y_node);
+      const int d_frontier_id = d_tree.addChild(d_root_id, rhs_q_ref_node);
+
+      frontier.push_back(d_frontier_id);
+
+      while(frontier.size() > 0)
+      {
+         const int d_node_id = frontier.back();
+         frontier.pop_back();
+
+         const auto d_node = d_tree[d_node_id];
+
+         if (d_node.nodeType != jymbo::types::enumDerivativeNodeType_t::kReference)
+         {
+            std::cout << "tried to expand a node that isn't a reference to the q-tree\n";
+            return;
+         }
+
+         
+      }
    }
 }
