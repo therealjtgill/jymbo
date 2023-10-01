1d47c344021298db081b93a77a376d978b0d069b
Jonathan Gill <therealjtgill@gmail.com>
2023-10-01 17:35:04 -0400

    #24 - Add derivative subtrees for trig functions (#29)
    * Added declarations and blank definitions for trig derivative subtrees.
    
    * Added provisional implementations for sine and cosine derivatives.
    
    * Added the tangent derivative subtree.
    
    * Added sine, cosine, and tangent derivative subtrees to the map in the derivatizer.
    
    * Added a test that takes the derivative of sine.
    
    * Fixed a bug where the '-2' term wasn't added as a child of the 'pow' operator in the tangent derivative subtree.
    
    * Added a test for tangent derivatives.
    
    * Made the tangent derivative use a reference node for the right-child of the tangent operator.

--- jymbo/include/operators.hpp
+++ jymbo/include/operators.hpp
@@ -47,6 +47,24 @@ namespace jymbo
       jymbo::types::DerivativeTree & d_tree
    );
 
+   jymbo::types::derivativeFrontierNodes sineDerivativeSubtree(
+      const int d_node_id,
+      const jymbo::types::QueryTree & q_tree,
+      jymbo::types::DerivativeTree & d_tree
+   );
+
+   jymbo::types::derivativeFrontierNodes cosineDerivativeSubtree(
+      const int d_node_id,
+      const jymbo::types::QueryTree & q_tree,
+      jymbo::types::DerivativeTree & d_tree
+   );
+
+   jymbo::types::derivativeFrontierNodes tangentDerivativeSubtree(
+      const int d_node_id,
+      const jymbo::types::QueryTree & q_tree,
+      jymbo::types::DerivativeTree & d_tree
+   );
+
    class Derivatizer
    {
       public:
@@ -71,7 +89,10 @@ namespace jymbo
             {jymbo::types::enumOperatorType_t::kSubtraction, subtractionDerivativeSubtree},
             {jymbo::types::enumOperatorType_t::kMultiplication, multiplicationDerivativeSubtree},
             {jymbo::types::enumOperatorType_t::kDivision, divisionDerivativeSubtree},
-            {jymbo::types::enumOperatorType_t::kPower, powerDerivativeSubtree}
+            {jymbo::types::enumOperatorType_t::kPower, powerDerivativeSubtree},
+            {jymbo::types::enumOperatorType_t::kSine, sineDerivativeSubtree},
+            {jymbo::types::enumOperatorType_t::kCosine, cosineDerivativeSubtree},
+            {jymbo::types::enumOperatorType_t::kTangent, tangentDerivativeSubtree}
          };
    };
 }
